f0918ea314affce0df0e471f287754bfa4d66709
k-jaehyun <wogus146@gmail.com>
2023-12-27 19:48:00 +0900

    Feat: Validation exception 추가

---
+++ src/main/java/com/example/trello/global/exception/fieldError/FieldErrorException.java
@@ -0,0 +1,16 @@
+package com.example.trello.global.exception.fieldError;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class FieldErrorException extends RuntimeException{
+    private final Integer status;
+    private final List<FieldErrorDto> fieldErrorDtoList;
+    public FieldErrorException(String message, Integer statusCode, List<FieldErrorDto> fieldErrorDtoList) {
+        super(message);
+        this.status=statusCode;
+        this.fieldErrorDtoList=fieldErrorDtoList;
+    }
+}
