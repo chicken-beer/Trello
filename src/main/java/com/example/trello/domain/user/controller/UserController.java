b4a7b303549fa1efbd466abc9df54944fe51992d
k-jaehyun <wogus146@gmail.com>
2023-12-28 10:36:57 +0900

    Feat : validation exception 삭제

--- src/main/java/com/example/trello/domain/user/controller/UserController.java
+++ src/main/java/com/example/trello/domain/user/controller/UserController.java
@@ -2,8 +2,6 @@ package com.example.trello.domain.user.controller;
 
 import com.example.trello.domain.user.dto.UserSignupDto;
 import com.example.trello.domain.user.service.UserService;
-import com.example.trello.global.exception.fieldError.FieldErrorDto;
-import com.example.trello.global.exception.fieldError.FieldErrorException;
 import com.example.trello.global.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -23,17 +21,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/auth/signup")
-    public ResponseEntity signup(@Valid @RequestBody UserSignupDto request, BindingResult bindingResult) {
-        // validation 검증
-        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-        if(fieldErrors.size() > 0) {
-            List<FieldErrorDto> fieldErrorDtoList =fieldErrors.stream().map(FieldErrorDto::new).toList();
-            throw new FieldErrorException(
-                    "허용된 입력값이 아닙니다.",
-                    HttpStatus.BAD_REQUEST.value(),
-                    fieldErrorDtoList);
-        }
-
+    public ResponseEntity signup(@Valid @RequestBody UserSignupDto request) {
         userService.signup(request);
         return ResponseEntity.ok( ApiResponse.ok("회원가입에 성공하셨습니다.") );
     }
