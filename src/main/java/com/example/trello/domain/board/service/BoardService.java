96214275cf4f8f91e71272d6b4d966a2977104e1
Yoon <dlstjr9390@gmail.com>
2023-12-30 19:49:52 +0900

    feat : 첨부파일 변경 및 삭제

--- src/main/java/com/example/trello/domain/board/service/BoardService.java
+++ src/main/java/com/example/trello/domain/board/service/BoardService.java
@@ -26,7 +26,6 @@ public class BoardService {
 
     private final BoardRepository boardRepository;
     private final BoardUsersRepository boardUsersRepository;
-    private final UserRepository userRepository;
     private final S3Utils s3Utils;
 
     public String createBoard(BoardRequestDto boardRequestDto, User user){
@@ -75,7 +74,11 @@ public class BoardService {
         if(!boardUsers.getUserRole().equals("Admin")){
             throw new CustomException(HttpStatus.CONFLICT,"권한이 없습니다.");
         }
+        String filename = board.getFilename();
+        s3Utils.deleteFile(filename);
 
+        String newFilename = s3Utils.uploadFile(boardRequestDto.getBackImg());
+        boardRequestDto.setFilename(newFilename);
         board.update(boardRequestDto);
         return "보드 수정 완료";
     }
@@ -90,62 +93,10 @@ public class BoardService {
             throw new CustomException(HttpStatus.CONFLICT,"권한이 없습니다.");
         }
 
+        String filename = board.getFilename();
+        s3Utils.deleteFile(filename);
         boardRepository.delete(board);
+
         return "게시물 삭제 완료";
-    }
-
-    public String inviteUser(Long boardId, String[] usernameList, User user) {
-        Board board = boardRepository.findById(boardId).orElseThrow(
-                ()-> new NoSuchElementException("보드를 찾을 수 없습니다.")
-        );
-        if(boardUsersRepository.findByUserId((user.getId())).isEmpty()){
-            throw new CustomException(HttpStatus.CONFLICT, "권한이 없습니다.");
-        } else if(!boardUsersRepository.findByBoardIdAndUserId(boardId,user.getId()).getUserRole().equals("Admin")){
-            throw new CustomException(HttpStatus.CONFLICT,"권한이 없습니다.");
-        }
-
-        for(int i=0; i< usernameList.length; i++){
-            User invitedUser = userRepository.findByUsername(usernameList[i]).orElseThrow(
-                    ()-> new NoSuchElementException("사용자를 찾을 수 없습니다.")
-            );
-            boardUsersRepository.save(BoardUsers.builder()
-                    .board(board)
-                    .user(invitedUser)
-                    .userRole("invitedMember")
-                    .build()
-            );
-        }
-        return "초대가 완료되었습니다.";
-    }
-
-    public void responseInvite(Long boardId, String response, User user) {
-        BoardUsers boardUsers = boardUsersRepository.findByBoardIdAndUserId(boardId, user.getId());
-        if(boardUsers == null){
-            throw new CustomException(HttpStatus.CONFLICT,"보드가 삭제됐거나 초대가 취소되었습니다.");
-        }
-        if(boardUsers.getUserRole().equals("invitedMember")){
-            if(response.equals("Accept")){
-                boardUsers.updateUserRole("Member");
-            } else if(response.equals("Refuse")){
-                boardUsersRepository.delete(boardUsers);
-            } else{
-                throw new CustomException(HttpStatus.CONFLICT,"잘못된 응답입니다.");
-            }
-        } else{
-            throw new CustomException(HttpStatus.CONFLICT, "유효하지 않은 초대입니다.");
-        }
-    }
-
-    public void changeUserRole(Long boardId, Long userId, String userRole, User user) {
-        Board board = boardRepository.findById(boardId).orElseThrow(
-                ()-> new NoSuchElementException("존재하지 않는 보드입니다.")
-        );
-        if(!board.getCreator().equals(user.getLoginId())){
-            throw new CustomException(HttpStatus.CONFLICT, "권한이 없습니다.");
-        }
-
-        BoardUsers boardUsers = boardUsersRepository.findByBoardIdAndUserId(boardId,userId);
-        boardUsers.updateUserRole(userRole);
-
     }
 }
