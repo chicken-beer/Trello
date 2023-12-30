7b0d6741972d6742978b20cbc6b1e7ccaea1e2e6
Yoon <dlstjr9390@gmail.com>
2023-12-30 22:11:15 +0900

    refactor : file에 관한 필드 수정

--- src/main/java/com/example/trello/domain/board/entity/Board.java
+++ src/main/java/com/example/trello/domain/board/entity/Board.java
@@ -2,6 +2,7 @@ package com.example.trello.domain.board.entity;
 
 import com.example.trello.domain.board.dto.BoardRequestDto;
 import com.example.trello.domain.boardUsers.entity.BoardUsers;
+import com.example.trello.domain.column.entity.Columns;
 import com.example.trello.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -35,16 +36,30 @@ public class Board {
     @OneToMany(mappedBy = "board" , cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BoardUsers> boardUserList = new ArrayList<>();
 
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Columns> columnsList = new ArrayList<>();
+
     public Board(BoardRequestDto boardRequestDto, User user) {
         this.creator = user.getLoginId();
         this.title = boardRequestDto.getTitle();
-        this.filename = boardRequestDto.getFilename();
+        this.description = boardRequestDto.getDescription();
+    }
+
+    public Board(BoardRequestDto boardRequestDto, User user, String filename) {
+        this.creator = user.getLoginId();
+        this.title = boardRequestDto.getTitle();
+        this.filename = filename;
+        this.description = boardRequestDto.getDescription();
+    }
+
+    public void update(BoardRequestDto boardRequestDto, String filename) {
+        this.title = boardRequestDto.getTitle();
+        this.filename = filename;
         this.description = boardRequestDto.getDescription();
     }
 
     public void update(BoardRequestDto boardRequestDto) {
         this.title = boardRequestDto.getTitle();
-        this.filename = boardRequestDto.getFilename();
         this.description = boardRequestDto.getDescription();
     }
 
